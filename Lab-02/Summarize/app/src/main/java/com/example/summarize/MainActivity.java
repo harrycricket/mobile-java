6db3bb214beacd9ff809b8c51bf4adf5ae6599cc
Harry <phuothuynh2002@gmail.com>
2024-06-06 01:07:40 +0700

    Combine all project in lab 2

---
+++ Lab-02/Summarize/app/src/main/java/com/example/summarize/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.summarize;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button calculate, random, signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        calculate = (Button) findViewById(R.id.calculate);
+        random = (Button) findViewById(R.id.random);
+        signIn = (Button) findViewById(R.id.signIn);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RandomActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
